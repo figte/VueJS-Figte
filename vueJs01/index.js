ac053ee4f7b4885ad6ea744d7feb83e552979ee9
Figte <walther.fig@gmail.com>
2020-06-19 15:49:44 -0600

    pratica uno, con sintaxis, estructura y elementos basicos

---
+++ vueJs01/index.js
@@ -0,0 +1,29 @@
+const app=new Vue({ 
+    el:"#app",
+    data:{
+        saludo:"Hola a todos!",
+        productos:[],
+        usuarios:[],
+        n1:0,
+        n2:0,
+        respuesta:0
+        
+
+    },
+    methods: {
+        
+    },
+    created() {
+
+        //peticion para obtener un listado de usuario
+        //asignandolo a la variable usuarios.
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response=>response.json())
+        .then(json=>{
+            this.usuarios=json;
+        })
+
+    }
+});
+
+
